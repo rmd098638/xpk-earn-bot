ae7eb61f65185cbd19b6bca00661b763085a1843
rmd098638 <rmd098638@gmail.com>
2026-07-06 23:19:35 +0600

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,12 +1,8 @@
 import os
 import firebase_admin
-
-from firebase_admin import credentials
-from firebase_admin import firestore
-
+from firebase_admin import credentials, firestore
 
 db = None
-
 
 def initialize_firebase():
     global db
@@ -23,9 +19,14 @@ def initialize_firebase():
     cred = credentials.Certificate({
         "type": "service_account",
         "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": "",
         "private_key": private_key,
         "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        "token_uri": "https://oauth2.googleapis.com/token"
+        "client_id": "",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": ""
     })
 
     firebase_admin.initialize_app(cred)
